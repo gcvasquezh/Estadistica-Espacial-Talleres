e0fa5524f516a78f64f23de078e7350dc29ad505
Andersong98 <jaguarinl@unal.edu.co>
2024-03-14 20:49:55 -0500

    .

--- Taller clase 13-03-24.R
+++ Taller clase 13-03-24.R
@@ -1,14 +1,13 @@
 # Taller Estadística Espacial - Geoestadística
 # Nombre: John Anderson Guarin Lopez - German Camilo Vasquez Herrera
 
-
 library(geoR)
 library(MASS)
 library(gstat)
 library(readxl)
 
 #Punto 1##############
-setwd("C:/Users/gcvh2/Documents/UNAL/8. 2024 -1S/Estadística Espacial/Estadistica-Espacial-Talleres")
+setwd("C:/Users/ander/OneDrive - Universidad Nacional de Colombia/Documentos/(2024-01) Octavo Semestre/Estadística Espacial/Estadistica-Espacial-Talleres")
 superficie<-read_excel("Superficie.xlsx")
 fondo<-read_excel("Fondo.xlsx")
 summary(superficie)
@@ -32,18 +31,17 @@ plot(variograma, env=variograma.env)
 
 variofit(variograma, ini.cov.pars = )
 
-
 # Fondo - Salinidad ####
-vario_fsali<-variog(coords=Fondo[,c("X","Y")],data=Fondo$Sali,uvec=seq(0,1000,length=50),estimator.type="class")
+vario_fsali<-variog(coords=fondo[,c("X","Y")],data=fondo$Sali,uvec=seq(0,1000,length=50),estimator.type="class")
 plot(vario_fsali)
 
-fsali_esferico<-variofit(vario_fsali,ini=c(sigmasq=varianza_fsali,phi=100),fix.nugget=FALSE,cov.model="spherical")
-fsali_lineal<-variofit(vario_fsali,ini=c(sigmasq=varianza_fsali,phi=100),fix.nugget=FALSE,cov.model="linear")
-fsali_exponencial<-variofit(vario_fsali,ini=c(sigmasq=varianza_fsali,phi=100),fix.nugget=FALSE,cov.model="exponential")
+fsali_esferico<-variofit(vario_fsali,ini=c(sigmasq=var(fondo[,3]),phi=0),fix.nugget=FALSE,cov.model="spherical")
+fsali_lineal<-variofit(vario_fsali,ini=c(sigmasq=var(fondo[,3]),phi=0),fix.nugget=FALSE,cov.model="linear")
+fsali_exponencial<-variofit(vario_fsali,ini=c(sigmasq=var(fondo[,3]),phi=0),fix.nugget=FALSE,cov.model="exponential")
 
-R2_fsali_esferico<-1-summary(fsali_esferico)$sum.of.squares/varianza_fsali
-R2_fsali_lineal<-1-summary(fsali_lineal)$sum.of.squares/varianza_fsali
-R2_fsali_exponencial<-1-summary(fsali_exponencial)$sum.of.squares/varianza_fsali
+R2_fsali_esferico<-1-summary(fsali_esferico)$sum.of.squares/var(fondo[,3])
+R2_fsali_lineal<-1-summary(fsali_lineal)$sum.of.squares/var(fondo[,3])
+R2_fsali_exponencial<-1-summary(fsali_exponencial)$sum.of.squares/var(fondo[,3])
 
 print(paste("R^2 del modelo esférico:", R2_fsali_esferico))
 print(paste("R^2 del modelo lineal:", R2_fsali_lineal)) # Mejor de todos
@@ -52,5 +50,25 @@ print(paste("R^2 del modelo exponencial:", R2_fsali_exponencial))
 fsali_pepita<-fsali_lineal$nugget;fsali_pepita
 fsali_meseta<-fsali_lineal$nugget+summary(fsali_lineal)$estimated.pars[2];fsali_meseta
 fsali_rango<-summary(fsali_lineal)$estimated.pars[3];fsali_rango
+
+# Fondo - Oxigeno ####
+vario_foxi<-variog(coords=fondo[,c("X","Y")],data=fondo$oxig,uvec=seq(0,1000,length=50),estimator.type="class")
+plot(vario_foxi)
+
+foxi_esferico<-variofit(vario_foxi,ini=c(sigmasq=var(fondo[,4]),phi=0),fix.nugget=FALSE,cov.model="spherical")
+foxi_lineal<-variofit(vario_foxi,ini=c(sigmasq=var(fondo[,4]),phi=0),fix.nugget=FALSE,cov.model="linear")
+foxi_exponencial<-variofit(vario_foxi,ini=c(sigmasq=var(fondo[,4]),phi=0),fix.nugget=FALSE,cov.model="exponential")
+
+R2_foxi_esferico<-1-summary(foxi_esferico)$sum.of.squares/var(fondo[,4])
+R2_foxi_lineal<-1-summary(foxi_lineal)$sum.of.squares/var(fondo[,4])
+R2_foxi_exponencial<-1-summary(foxi_exponencial)$sum.of.squares/var(fondo[,4])
+
+print(paste("R^2 del modelo esférico:", R2_foxi_esferico))
+print(paste("R^2 del modelo lineal:", R2_foxi_lineal)) # Mejor de todos
+print(paste("R^2 del modelo exponencial:", R2_foxi_exponencial))
+
+foxi_pepita<-foxi_lineal$nugget;foxi_pepita
+foxi_meseta<-foxi_lineal$nugget+summary(foxi_lineal)$estimated.pars[2];foxi_meseta
+foxi_rango<-summary(foxi_lineal)$estimated.pars[3];foxi_rango
 
 
