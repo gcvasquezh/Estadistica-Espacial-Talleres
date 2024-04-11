4eb360be078b1e27e4cd96a8cdc6a81a92e2ab59
Andersong98 <jaguarinl@unal.edu.co>
2024-04-10 19:48:39 -0500

    .

--- Ejercicio Juguete.R
+++ Ejercicio Juguete.R
@@ -1,38 +1,42 @@
+#### Ejercicio Predicción Kriging - Estadística Espacial ####
+
+## Nombre: 
+
 library(geoR)
 library (gstat)
 library(MASS)
-coordenadas<-cbind(c(1,1,1,2,2,3,3),c(1,2,3,1,3,2,3),c(12,15,10,14,NA,12,NA,14,13))
-colnames(coordenadas)<-c("X","Y","Datos")
-daticos<-c(12,15,10,14,12,14,13)
+library(ade4)
+
 grid<-expand.grid(seq(0,2),seq(0,2))
 plot(grid)
+
 distancia<-dist(grid)
 distancia<-as.matrix(distancia)
-sigma<-var(daticos)
-covarianza<-sigma-2.81*(1-exp(-distancia/2))
-covarianza<-as.matrix(covarianza)
-matplot(distancia,covarianza)
 
-medias.cte<-rep(mean(daticos),9)
-normal.cte<-mvrnorm(1,medias.cte,covarianza)
-datoscte<-cbind(grid[,1],grid[,2],normal.cte)
+datos <- c(12,14,NA,15,NA,14,10,12,13)
+coordenadas<-cbind(grid[,1],grid[,2],datos)
+colnames(coordenadas)<-c("X","Y","Datos")
+coordenadas <- as.geodata(coordenadas,coords=1:2,var=3)
+plot(coordenadas)
 
-x<-c(0,2,2,0,0)
-y<-c(0,0,2,2,0)
-datoscte.borde<-cbind(x,y)
-datoscte.grid<-expand.grid(Este=seq(0,2), Norte=seq(0,2))
-plot(datoscte.grid)
+variograma<-2.81*(1-exp(-distancia/2))
 
-vario_toy<-variog(coords=coordenadas[,c("X","Y")],data=coordenadas[,3],estimator.type="class")
-plot(vario_toy)
-lineal<-variofit(vario_toy,ini=c(sigmasq=var(daticos),phi=1),fix.nugget=FALSE,cov.model="linear")
-pepita<-lineal$nugget
-meseta<-lineal$nugget+summary(lineal)$estimated.pars[2]
-rango<-summary(lineal)$estimated.pars[3]
+covariograma<-var(na.omit(coordenadas$data))-variograma
 
-coord<-cbind(c(1,1,1,2,2,2,3,3,3),c(1,2,3,1,2,3,1,2,3),c(12,15,10,14,NA,12,NA,14,13))
-datoscte.kc<-krige.conv(datoscte,coords=datoscte[,c(1,2)], 
-                        data = datoscte[,3] ,locations=datoscte.grid, 
-                        bord=datoscte.borde, krige= krige.control(nugget=pepita,
-                        cov.pars=c(sigmasq=meseta, phi= rango)))
+# Matriz C_ij
+C.ij<-covariograma[-c(3,5),-c(3,5)]
+
+# Vector C_i0
+C.i0<-covariograma[-c(3,5),5]
+
+# Pesos lambda
+lambda<-ginv(C.ij)%*%C.i0
+
+# Datos sin los NA's
+datossin<-datos[-c(3,5)]
+
+# Estimación
+t(lambda)%*%datossin
+
+
 
--- Tarea Mantel/Ejercicio Mantel.R
+++ Tarea Mantel/Ejercicio Mantel.R
@@ -1,24 +1,55 @@
-library (ade4)
-library(readr)
+#### Test de Mantel - Temperatura Bogotá 2023 ####
+
+# Integrantes:
+# John Anderson Guarin Lopez
+# German Camilo Vasquez Herrera
+
+# Estadística Espacial
+
+# Librerías Utilizadas ----------------------------------------------------
+
+if (!require(ade4)){install.packages("ade4");library(ade4)}
+if (!require(readr)){install.packages("readr");library(readr)}
+if (!require(readxl)){install.packages("readxl");library(readxl)}
+if (!require(dplyr)){install.packages("dplyr");library(dplyr)}
+
+# Fijar Directorio y Carga de Base de Datos -------------------------------
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 getwd()
 temp<-read.csv("temperatura.csv", sep=",", header=T)
 temp<-temp[,c(3,4,18)]
 
+est <- read_excel("CNE_IDEAM.xls", col_names = TRUE)
+est <- est[, 9:12]
+est[, 1:2] <- lapply(est[, 1:2], function(x) as.numeric(as.character(x)))
+est_1 <- est %>% filter(est[,3] == "Bogota")
+
+# Verificación de Datos Faltantes -----------------------------------------
+
 missings <- function(x) return(sum(is.na(x)))
 apply(temp,2,missings)
+apply(est_1,2,missings)
 
-# Mantel test
-?mantel.rtest
+# Mapa de Estaciones en Bogotá --------------------------------------------
+
+plot(x = est_1$LONGITUD, y = est_1$LATITUD, xlab = "Longitud", ylab = "Latitud",
+     type = "l", col = 4, lwd = 2, main = "Estaciones en Bogotá")
+points(temp[,2], temp[,1], col=2, pch=16, 
+       main="Estaciones de Monitoreo de Temperatura en Bogotá",
+       xlab="Longitud", ylab="Latitud")
+
+# Test de Mantel ----------------------------------------------------------
+
 coord_dists <- dist(cbind(temp$Longitud, temp$Latitud))
 valor_dists <- dist(temp$Valor)
 tictoc::tic()
 res<-mantel.rtest(coord_dists, valor_dists, nrepet = 1000)
 tictoc::toc()
 res
-plot(res, xlab="M", main="Montecarlo distribution")
 
-#Ya que el p-vaor es tan pequeño rechazamos la hipótesis nula
-#y decimos que si hay aleatoriedad espacial
+plot(res, xlab="M", main="Distribución de Montecarlo Test de Mantel")
+
+#Ya que el p-valor es tan pequeño rechazamos la hipótesis nula
+#y decimos que si hay correlación espacial
 
